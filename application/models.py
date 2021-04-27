72918d6822fe51d3544d83ce9ddf1ee290647a55
Michaela Gasteratou <elagasteratou@gmail.com>
2021-04-27 18:36:47 +0100

    Rebased, like feature on the blog now working

--- application/models.py
+++ application/models.py
@@ -16,6 +16,7 @@ class Song(db.Model):
     song_id = db.Column(db.Integer, primary_key=True)
     contestant_id = db.Column(db.Integer, db.ForeignKey("contestant.contestant_id"), nullable=False)
     song_name = db.Column(db.String(64), nullable=False)
+    song_likes = db.Column(db.Integer, nullable=True)
 
 
 class Blog(db.Model):
@@ -25,6 +26,7 @@ class Blog(db.Model):
     blog_views = db.Column(db.Integer, nullable=False)
     blog_title = db.Column(db.String(100), nullable=False)
     blog_text = db.Column(db.String(1000), nullable=False)
+    blog_likes = db.Column(db.Integer, nullable=True)
 
 class Contact(db.Model):
     contact_id = db.Column(db.Integer, primary_key=True)
